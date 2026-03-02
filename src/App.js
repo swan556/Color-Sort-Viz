6cd34a5b58f2f1c97e8696e34680354cbd4f7737
Swan <swanand556@gmail.com>
2026-03-02 23:09:15 +0530

    basic structure done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import { useState } from "react";
+import AlgoSelectionBoard from "./componants/SelectAlgorithms/AlgoSelectionBoard";
+import ColorBoard from "./componants/CreateArray/CustomArraySelector/ColorBoard";
+import RandomArrayGenerator from "./componants/CreateArray/RandomArrayGenerator";
+import Button from "./componants/Button";
 import "./index.css";
 
 export default function App() {
   const [algoSelected, setAlgoSelected] = useState(false);
-  return <>{algoSelected ? <></> : <></>}</>;
+
+  function handlePageSelected() {
+    setAlgoSelected((al) => !al);
+  }
+
+  return (
+    <>
+      {algoSelected ? (
+        <>
+          <AlgoSelectionBoard />
+          <div className="array-selector">
+            <ColorBoard />
+            <RandomArrayGenerator />
+          </div>
+          <Button onClick={handlePageSelected} />
+        </>
+      ) : (
+        <>
+          <p>in construction</p>
+        </>
+      )}
+    </>
+  );
 }
